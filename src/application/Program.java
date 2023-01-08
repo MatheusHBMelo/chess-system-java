1b761ec3ecdf00924b2259ce868d2f867ece14ab
MatheusHBMelo <matheushbmelo@gmail.com>
2023-01-08 10:14:53 -0300

    [Refact] Printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,10 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Piece;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        // Teste da classe de Position
-        Position pos = new Position(3, 10);
-        System.out.println(pos);
-
-        // Teste da classe de Board
-        Board board = new Board(8,8);
-
-        // Teste da classe de Piece
-        Piece piece = new Piece(board);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
