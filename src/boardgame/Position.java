54d4410f53e4acb3c147b67867785b40a0fec693
MatheusHBMelo <matheushbmelo@gmail.com>
2023-01-16 11:37:23 -0300

    [FEAT] Definitive class Position

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -4,9 +4,7 @@ public class Position {
     private int row;
     private int column;
 
-    public Position(){}
-
-    public Position(int row, int column){
+    public Position(int row, int column) {
         this.row = row;
         this.column = column;
     }
@@ -15,17 +13,25 @@ public class Position {
         return row;
     }
 
+    public void setRow(int row) {
+        this.row = row;
+    }
+
     public int getColumn() {
         return column;
     }
 
-    public void setValues(int row, int column){
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public void setValues(int row, int column) {
         this.row = row;
         this.column = column;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return row + ", " + column;
     }
 }
